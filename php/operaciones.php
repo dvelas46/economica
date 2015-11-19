469a6cf495549aea6f4d13624a7ff1023f4af53d
DIEGOO <DIEGOO@10.9.31.8>
2015-11-18 20:03:04 -0500

    actualizado varible fecha

--- php/operaciones.php
+++ php/operaciones.php
@@ -2,45 +2,47 @@
 
 if (isset($_POST["aceptar"])) {
 
-        $array = array();
+    $array = array();
 
-        $NOMBRE = $_POST["NOMBRE"];
-        $APELLIDO = $_POST["APELLIDO"];
-        $cc = $_POST["CC"];
-        $p = floatval($_POST["PRESTAMO"]);
-        $PLAZO = floatval($_POST["PLAZO"]);
-        $periodo = $_POST["periodo"];
-        $tn = floatval($_POST["tn"]);
-        $ea = floatval($_POST["ea"]);
-        $ip = floatval($_POST["ip"]) / 100;
-        $per;
+    $NOMBRE = $_POST["NOMBRE"];
+    $APELLIDO = $_POST["APELLIDO"];
+    $cc = $_POST["CC"];
+    $p = floatval($_POST["PRESTAMO"]);
+    $PLAZO = floatval($_POST["PLAZO"]);
+    $periodo = $_POST["periodo"];
+    $tn = floatval($_POST["tn"]);
+    $ea = floatval($_POST["ea"]);
+    $ip = floatval($_POST["ip"]) / 100;
+    $per;
 
-        switch ($periodo) {
-            case 'MENSUAL':
-                $per = 1;
-                break;
-            case 'BIMESTRAL':
-                $per = 2;
-                break;
-            case 'TRIMESTRAL':
-                $per = 3;
-                break;
-        }
+    switch ($periodo) {
+        case 'MENSUAL':
+            $per = 1;
+            break;
+        case 'BIMESTRAL':
+            $per = 2;
+            break;
+        case 'TRIMESTRAL':
+            $per = 3;
+            break;
+    }
 
     if ($_POST["linea"] == "1") {
-        
+
         $totalPeriodos = (12 / $per) * $PLAZO;
         $amortizacion = $p / $totalPeriodos;
 
+        $fecha = date('Y-m-d');
         for ($i = 0; $i <= $totalPeriodos; $i++) {
             $fila = new stdClass();
 
+            
             if ($i == 0) {
                 $seguro = ($p * 0.005) * $per;
                 $flujo = ($p - $seguro);
 
                 $fila->periodo = $i;
-                $fila->fecha = date('Y-m-d');
+                $fila->fecha = $fecha;
                 $fila->saldo = $p;
                 $fila->amortizacion = 0;
                 $fila->interes = 0;
@@ -56,8 +58,12 @@ if (isset($_POST["aceptar"])) {
                 $seguro = ($p * 0.005) * $per;
                 $flujo = (($amortizacion + $interes) + $seguro) * (-1);
 
+                
+                $fecha = strtotime('+'.$per.' month', strtotime($fecha));
+                $fecha = date('Y-m-d', $fecha);
+
                 $fila->periodo = $i;
-                $fila->fecha = date('Y-m-d');
+                $fila->fecha = $fecha;
                 $fila->saldo = $p;
                 $fila->amortizacion = $amortizacion;
                 $fila->interes = $interes;
